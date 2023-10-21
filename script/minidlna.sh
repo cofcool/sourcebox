588096adfeaa91a1e4ef91cbf1d5266e7996c413
CofCool <cofcool@outlook.com>
2023-10-21 08:56:58 +0800

    remove unused files

--- script/minidlna.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# auto mount hard disk,then start minidlna service, and the service will scan media file in the disk.
-
-mount /dev/sda5 /media/pi/backup
-if [$? -eq 0];then
-	service minidlna restart
-fi
